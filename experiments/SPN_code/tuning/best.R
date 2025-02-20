8d0921789e972a3ecd4beaef8b639281353eea77
José A. Rodríguez Gallego <96689554+RodGal-2020@users.noreply.github.com>
2025-02-20 15:00:58 +0100

    Increased compatibility with test mode

--- experiments/SPN_code/tuning/best.R
+++ experiments/SPN_code/tuning/best.R
@@ -2,8 +2,12 @@ aw_ranks <- all_workflows %>%
   workflowsets::rank_results(rank_metric = params$rank_metric, select_best = TRUE) %>%
   filter(.metric == params$rank_metric)
 
-aw_autplot <- all_workflows %>%
+aw_autoplot <- all_workflows %>%
   autoplot(metric = params$rank_metric, select_best = TRUE) +
   geom_text(aes(y = mean - 0.04, label = wflow_id), angle = 90, hjust = 1) +
-  lims(y = c(0.8, 0.95)) +
   theme(legend.position = "none")
+
+if (!params$test) {
+  aw_autoplot = aw_autoplot +
+    lims(y = c(0.8, 0.95))
+}
