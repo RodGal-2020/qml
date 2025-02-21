53a734f0d45347afbdcbb0285a7d1ca69035426d
José A. Rodríguez Gallego <96689554+RodGal-2020@users.noreply.github.com>
2025-02-21 14:00:58 +0100

    Avoid visualization error

--- experiments/SPN_code/tuning/best.R
+++ experiments/SPN_code/tuning/best.R
@@ -7,7 +7,7 @@ aw_autoplot <- all_workflows %>%
   geom_text(aes(y = mean - 0.04, label = wflow_id), angle = 90, hjust = 1) +
   theme(legend.position = "none")
 
-if (!params$test) {
-  aw_autoplot = aw_autoplot +
-    lims(y = c(0.8, 0.95))
-}
+# if (!params$test) {
+#   aw_autoplot = aw_autoplot +
+#     lims(y = c(0.8, 0.95))
+# }
