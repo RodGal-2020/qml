c5ab85bf4d52c6bbb6a28f38de3b3dd75dbb2e4a
RodGal-2020 <rodgal2020@gmail.com>
2025-10-13 10:19:43 -0400

    Fix formating using hat instead of the Spanish "gorro"

--- R/dmm.R
+++ R/dmm.R
@@ -1,6 +1,6 @@
 #' @title Quantum Matrix Method for Classification
 #'
-#' @description This function runs the Falcó algorithm for a given dataset. In the `falco.R` file we can find the kernel function, `K`; the `f_gorro_h` function, for the estimation of the probability of belonging to a class; and the `falco` function, which is the main function that runs the Falcó algorithm.
+#' @description This function runs the Falcó algorithm for a given dataset. In the `falco.R` file we can find the kernel function, `K`; the `f_hat_h` function, for the estimation of the probability of belonging to a class; and the `falco` function, which is the main function that runs the Falcó algorithm.
 #'
 #' `r lifecycle::badge("experimental")`
 #'
@@ -34,13 +34,13 @@ K <- function(..., r = 2) {
   sxx <- sum(x^2)
 
   if (sxx < 1) {
-    return(1/2 * (r + 2) * (1 - sxx))
+    return(1 / 2 * (r + 2) * (1 - sxx))
   } else {
     return(0)
   }
 }
 
-f_gorro_h <- function(D_tilde, x, clase, h_window = 0.1) {
+f_hat_h <- function(D_tilde, x, clase, h_window = 0.1) {
   # La D_tilde es la del entrenamiento
 
   # Para cada vector x esta función calcula la probabilidad estimada de pertenencia a la clase "clase", considerando además una ventana h_window.
@@ -49,7 +49,8 @@ f_gorro_h <- function(D_tilde, x, clase, h_window = 0.1) {
   # x = c(v_1 = 0.01, v_2 = 0.02)
   # x = D_tilde[[clase]] %>% summarize(mean(X_tilde_r_1), mean(X_tilde_r_2)) %>% unlist() # el promedio para la clase considerada
   # clase = 1; h_window = 0.1
-  x_1 <- x[1]; x_2 <- x[2]
+  x_1 <- x[1]
+  x_2 <- x[2]
   n_Data <- nrow(D_tilde %>% bind_rows()) # n es el tamaño muestral, confirmado por Falcó
 
   D_tilde[[clase]] %>% # Esto identifica los valores de X_tilde_r recogidos hasta el momento para la clase "clase"
@@ -66,7 +67,13 @@ f_gorro_h <- function(D_tilde, x, clase, h_window = 0.1) {
     divide_by(n_Data * h_window^2)
 }
 
-get_rho_d = function(Data, objective_var = NULL, n_breaks = 3, verbose = 0, test_var = "test") {
+get_rho_d = function(
+  Data,
+  objective_var = NULL,
+  n_breaks = 3,
+  verbose = 0,
+  test_var = "test"
+) {
   # Examples
   # Data = Data_list[[1]]; objective_var = "y"; n_breaks = 3; verbose = 3; test_var = "test"
 
@@ -102,16 +109,18 @@ get_rho_d = function(Data, objective_var = NULL, n_breaks = 3, verbose = 0, test
   # NOTA: OMITIMOS la variable objetivo, que al haber sido un factor sería transformada en este paso
   y = new_Data %>% select(all_of(objective_var), test)
   new_Data %<>%
-  # new_Data %>%
-  # select(6, y) %>%
+    # new_Data %>%
+    # select(6, y) %>%
     select(-all_of(objective_var), -test) %>%
-    map_if(is.factor, function(v) { # Ya no contamos con variables numéricas, sino que tenemos solo factores
+    map_if(is.factor, function(v) {
+      # Ya no contamos con variables numéricas, sino que tenemos solo factores
       l_v = levels(v) %>% length
-      v %>% map(function(x) {
-        # v_0 %>% replace(x, 1) %>% set_names(1:n_breaks) # Los nombres son un añadido para luego usar unnest_wider
-        # Tenemos que hacer como lo anterior pero sabiendo que puede haber varios niveles
-        rep(0, l_v) %>% replace(x, 1) %>% set_names(levels(v))
-      })
+      v %>%
+        map(function(x) {
+          # v_0 %>% replace(x, 1) %>% set_names(1:n_breaks) # Los nombres son un añadido para luego usar unnest_wider
+          # Tenemos que hacer como lo anterior pero sabiendo que puede haber varios niveles
+          rep(0, l_v) %>% replace(x, 1) %>% set_names(levels(v))
+        })
     }) %>%
     as_tibble() %>% # Lo hacemos debido a que map_if devuelve una lista
     bind_cols(y)
@@ -151,10 +160,12 @@ get_rho_d = function(Data, objective_var = NULL, n_breaks = 3, verbose = 0, test
   # 2. Separamos los datos en los conjuntos según la variable objetivo
   C <- list()
   for (i in 1:n_y_classes) {
-    C[[i]] = Data %>% filter(!!sym(new_objective[i]) == 1) %>% select(-starts_with(objective_var))
+    C[[i]] = Data %>%
+      filter(!!sym(new_objective[i]) == 1) %>%
+      select(-starts_with(objective_var))
   }
 
-  if(nrow(original_Data) != C %>% map(nrow) %>% unlist %>% sum) {
+  if (nrow(original_Data) != C %>% map(nrow) %>% unlist %>% sum) {
     stop("Error en la separación de los datos")
   }
 
@@ -162,7 +173,7 @@ get_rho_d = function(Data, objective_var = NULL, n_breaks = 3, verbose = 0, test
   test_C = C %>% map(~ .x %>% filter(test == 1) %>% select(-all_of(test_var)))
 
   train_C = C %>% map(~ .x %>% filter(test == 0) %>% select(-all_of(test_var)))
-    # bind_rows() %>%
+  # bind_rows() %>%
 
   # D = (f_1|...|f_k)
   D = train_C %>%
@@ -184,24 +195,41 @@ get_rho_d = function(Data, objective_var = NULL, n_breaks = 3, verbose = 0, test
 
   # This matrix is called a density matrix and it is a measure of quantum probability (also called non-classical probability).
   # That's because it has the following properties:
-  if (!(
-    # 1. It is a positive semi-definite matrix.
-    all(eigen(rho_d)$values >= 0 | eigen(rho_d)$values < 1e-15) & # Eigen devuelve los autovalores de la matriz
-    # 2. It has trace 1.
-    my_trace(rho_d) == 1 &
-    # 3. It is Hermitian (symmetric in this case).
-    all(rho_d == t(rho_d))
-  )) {
-    warning("La matriz rho_d no cumple con las propiedades de una matriz de densidad cuántica")
+  if (
+    !(
+      # 1. It is a positive semi-definite matrix.
+      all(eigen(rho_d)$values >= 0 | eigen(rho_d)$values < 1e-15) & # Eigen devuelve los autovalores de la matriz
+        # 2. It has trace 1.
+        my_trace(rho_d) == 1 &
+        # 3. It is Hermitian (symmetric in this case).
+        all(rho_d == t(rho_d))
+    )
+  ) {
+    warning(
+      "La matriz rho_d no cumple con las propiedades de una matriz de densidad cuántica"
+    )
   }
 
   return(list(rho_d = rho_d, train_C = train_C, test_C = test_C))
 }
 
-get_C_tilde_polar = function(Data, objective_var = NULL, n_breaks = 3, verbose = 0, test_var = "test", zero_limit = 4) {
+get_C_tilde_polar = function(
+  Data,
+  objective_var = NULL,
+  n_breaks = 3,
+  verbose = 0,
+  test_var = "test",
+  zero_limit = 4
+) {
   # Example
   # Data = Data_list[[1]]; n_breaks = 3; objective_var = "y"; verbose = 3; test_var = "test"; zero_limit = 4
-  rho_d_info <- get_rho_d(Data, objective_var, n_breaks = 3, verbose = 0, test_var = test_var)
+  rho_d_info <- get_rho_d(
+    Data,
+    objective_var,
+    n_breaks = 3,
+    verbose = 0,
+    test_var = test_var
+  )
 
   rho_d = rho_d_info$rho_d
   train_C = rho_d_info$train_C
@@ -239,7 +267,7 @@ get_C_tilde_polar = function(Data, objective_var = NULL, n_breaks = 3, verbose =
     # Esto último simplemente cuenta el número de entradas no nulas en X_tilde
     q %<>% diag %>% magrittr::extract(1)
 
-    if(abs(q) < 1e-10) {
+    if (abs(q) < 1e-10) {
       warning("q is close to zero, returning original X_tilde")
       return(X_tilde)
     }
@@ -259,71 +287,79 @@ get_C_tilde_polar = function(Data, objective_var = NULL, n_breaks = 3, verbose =
   # Map to EACH ROW in each train_C the get_X_tilde and then the get_U_coords:
   # train_C[[1]][1,] %>% get_X_tilde %>% get_U_coords(U = U) # Ejemplo de uso
   # train_C[[1]] %>% apply(get_X_tilde, MARGIN = 1) # Debería funcionar pero no lo hace, cosa que se puede identificar al ver el número de unos
-  train_C_tilde <- train_C %>% map(
-   ~ {
-     xp <- matrix(NA, nrow = 0, ncol = rank_rho)
-     for (r in 1:nrow(.x)) {
-       xp %<>% rbind(
-         .x[r, ] %>% get_X_tilde() %>% get_U_coords(U = U)
-       )
-     }
-     xp
-   }
- )
+  train_C_tilde <- train_C %>%
+    map(
+      ~ {
+        xp <- matrix(NA, nrow = 0, ncol = rank_rho)
+        for (r in 1:nrow(.x)) {
+          xp %<>%
+            rbind(
+              .x[r, ] %>% get_X_tilde() %>% get_U_coords(U = U)
+            )
+        }
+        xp
+      }
+    )
 
   if (verbose > 2) {
     cat("train_C_tilde:\n")
     train_C_tilde # Mismo espíritu q D_tilde pero en polares
   }
 
-  test_C_tilde <- test_C %>% map(
-   ~ {
-     xp <- matrix(NA, nrow = 0, ncol = rank_rho)
-     for (r in 1:nrow(.x)) {
-       xp %<>% rbind(
-         .x[r, ] %>% get_X_tilde() %>% get_U_coords(U = U)
-       )
-     }
-     xp
-   }
- )
+  test_C_tilde <- test_C %>%
+    map(
+      ~ {
+        xp <- matrix(NA, nrow = 0, ncol = rank_rho)
+        for (r in 1:nrow(.x)) {
+          xp %<>%
+            rbind(
+              .x[r, ] %>% get_X_tilde() %>% get_U_coords(U = U)
+            )
+        }
+        xp
+      }
+    )
 
   # X_tilde_r_e_y = \hat{X}_{r, e_y} = (theta|...|X_tilde_r|...|theta)
   # Como podemos ver, realmente esta información ya la tenemos guardada en el objeto train_C_tilde,
   # así que podemos ahorrarnos algunos pasos e ir directamente al grano
 
   if (rank_rho > 2) {
-    stop("El rango de rho_d es mayor a 2, y actualmente admitimos clasificación binaria únicamente")
+    stop(
+      "El rango de rho_d es mayor a 2, y actualmente admitimos clasificación binaria únicamente"
+    )
   }
 
   ## Coordenadas esféricas en R^r
   # Pasamos ahora cada vector de D_tilde a coordenadas esféricas
-  train_C_tilde_polar <- train_C_tilde %>% map(
-    ~ .x %>%
-      as_tibble() %>%
-      set_names(paste0("V_", 1:ncol(.))) %>% # Esta V realmente representa X_tilde_r
-      mutate(
-        r = sqrt(V_1^2 + V_2^2),
-        phi_1 = atan(V_2 / V_1)
-      ) %>%
-      select(r, phi_1)
-  )
+  train_C_tilde_polar <- train_C_tilde %>%
+    map(
+      ~ .x %>%
+        as_tibble() %>%
+        set_names(paste0("V_", 1:ncol(.))) %>% # Esta V realmente representa X_tilde_r
+        mutate(
+          r = sqrt(V_1^2 + V_2^2),
+          phi_1 = atan(V_2 / V_1)
+        ) %>%
+        select(r, phi_1)
+    )
 
   if (verbose > 2) {
     cat("train_C_tilde_polar:\n")
     train_C_tilde_polar
   }
 
-  test_C_tilde_polar <- test_C_tilde %>% map(
-    ~ .x %>%
-      as_tibble() %>%
-      set_names(paste0("V_", 1:ncol(.))) %>% # Esta V realmente representa X_tilde_r
-      mutate(
-        r = sqrt(V_1^2 + V_2^2),
-        phi_1 = atan(V_2 / V_1)
-      ) %>%
-      select(r, phi_1)
-  )
+  test_C_tilde_polar <- test_C_tilde %>%
+    map(
+      ~ .x %>%
+        as_tibble() %>%
+        set_names(paste0("V_", 1:ncol(.))) %>% # Esta V realmente representa X_tilde_r
+        mutate(
+          r = sqrt(V_1^2 + V_2^2),
+          phi_1 = atan(V_2 / V_1)
+        ) %>%
+        select(r, phi_1)
+    )
 
   ###### CASO DE CLASIFICACIÓN BINARIA
 
@@ -339,17 +375,35 @@ get_C_tilde_polar = function(Data, objective_var = NULL, n_breaks = 3, verbose =
   #   warning("Las distribuciones son iguales. Posiblemente nada funcione\n")
   # }
 
-  ff.test = fasano.franceschini.test::fasano.franceschini.test(train_C_tilde_polar[[1]], train_C_tilde_polar[[2]]) # Funciona, pues es multivariante
+  ff.test = fasano.franceschini.test::fasano.franceschini.test(
+    train_C_tilde_polar[[1]],
+    train_C_tilde_polar[[2]]
+  ) # Funciona, pues es multivariante
   if (!(ff.test$p.value < 0.05)) {
-    warning("No hay evidencia significativa de diferencia entre las distribuciones. Posiblemente nada funcione\n")
+    warning(
+      "No hay evidencia significativa de diferencia entre las distribuciones. Posiblemente nada funcione\n"
+    )
   } else {
     return(list(train = train_C_tilde_polar, test = test_C_tilde_polar))
   }
 }
 
-dmm = function(Data, n_breaks = 3, objective_var = "y", verbose = 0, test_var = "test", debug = FALSE) {
+dmm = function(
+  Data,
+  n_breaks = 3,
+  objective_var = "y",
+  verbose = 0,
+  test_var = "test",
+  debug = FALSE
+) {
   # Data = Data_list[[1]]; n_breaks = 3; objective_var = "y"; verbose = 3; test_var = "test"
-  C_tilde_polar <- get_C_tilde_polar(Data, objective_var = objective_var, n_breaks = n_breaks, verbose = verbose, test_var = "test")
+  C_tilde_polar <- get_C_tilde_polar(
+    Data,
+    objective_var = objective_var,
+    n_breaks = n_breaks,
+    verbose = verbose,
+    test_var = "test"
+  )
 
   D_tilde_train <- C_tilde_polar$train # Una entrada por clase
   D_tilde_test <- C_tilde_polar$test # Una entrada por clase
@@ -358,24 +412,27 @@ dmm = function(Data, n_breaks = 3, objective_var = "y", verbose = 0, test_var = 
     # x = c(0.01, 0.02)
     clases = 1:2 # Solo admitimos clasificación binaria actualmente
     new_class = clases %>%
-      map_dbl(~ f_gorro_h(D_tilde, x, clase = .x)
-      ) %>%
+      map_dbl(~ f_hat_h(D_tilde, x, clase = .x)) %>%
       which.max() # Hacemos la estimación basada en verosimilitud
     return(new_class)
   }
 
   return(
-    D_tilde_test %>% imap(
-    ~ .x %>% mutate(
-      .pred = map_dbl(
-        .x = pmap_dbl(select(., r, phi_1), ~ c(...) %>% classify(D_tilde = D_tilde_train)),
-        .f = ~ .x
-      ),
-      .truth = .y
-    ) %>%
-      mutate(across(c(.pred, .truth), as.factor))
-  ) %>%
-    bind_rows()
+    D_tilde_test %>%
+      imap(
+        ~ .x %>%
+          mutate(
+            .pred = map_dbl(
+              .x = pmap_dbl(
+                select(., r, phi_1),
+                ~ c(...) %>% classify(D_tilde = D_tilde_train)
+              ),
+              .f = ~.x
+            ),
+            .truth = .y
+          ) %>%
+          mutate(across(c(.pred, .truth), as.factor))
+      ) %>%
+      bind_rows()
   )
 }
-
